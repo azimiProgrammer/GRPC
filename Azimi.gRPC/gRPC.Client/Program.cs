9e29dc3737186a10c0be3191f0767800e5229f32
azimi_programer <azimi_programer@yahoo.com>
2021-03-25 15:00:44 +0330

    Unary

---
+++ Azimi.gRPC/gRPC.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using GrpcService.Server;
+using System;
+using System.Threading.Tasks;
+
+namespace gRPC.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //create chanel
+            using var chanel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            //create client
+            var client = new Greeter.GreeterClient(chanel);
+
+            var reply = await client.SayHelloAsync(new() { Name = "amir" });
+
+            Console.WriteLine($"reply : {reply.Message} ");
+            Console.WriteLine("press any key to exit ...");
+            Console.ReadKey();
+
+        }
+    }
+}
